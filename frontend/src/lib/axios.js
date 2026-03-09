82d8a1700821373f12e0805dfb75961e12fd95eb
siddhi-kalsulkar09 <chmcs2026.siddhi.kalsulkar@gmail.com>
2026-03-10 00:20:22 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const api = axios.create({
- baseURL: 'http://localhost:3000',
+ // baseURL: 'http://localhost:3000',
+ baseURL: 'https://eventmanagement-re4g.onrender.com',
 });
 
 
